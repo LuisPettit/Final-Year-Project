8ced1165b8c1986adc61df48016c3650c529dfe1
LuisPettit <62663487+LuisPettit@users.noreply.github.com>
2020-05-16 12:07:53 +0100

    Update combining_bot_accounts.py

--- Data Scrapers/combining_bot_accounts.py
+++ Data Scrapers/combining_bot_accounts.py
@@ -1,3 +1,5 @@
+# script that combines list of all bot accounts from different investigations
+
 import tweepy
 import time
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer 
@@ -6,12 +8,14 @@ import datetime
 import sqlite3
 import random
 
+# loads data from individual datasets
 pronbots = pd.read_csv('pronbots.csv', header=0, names=['user', 'label'])
 gilani = pd.read_csv('gilani-2017.csv', header=0, names=['user', 'label'])
 italy = pd.read_csv('cresci-rtbust-2019.csv', header=0, names=['user', 'label'])
 man_labelled = pd.read_csv('botometer-feedback-2019.csv', header=0, names=['user', 'label'])
 midterm = pd.read_csv('midterm-2018.csv', header=0, names=['user', 'label'])
 
+# only interested in bot accounts
 df_1 = italy[italy['label'] == 'bot']
 df_2 = man_labelled[man_labelled['label'] == 'bot']
 df_3 = gilani[gilani['label'] == 'bot']
@@ -21,11 +25,13 @@ df_5 = midterm[midterm['label'] == 'bot']
 dfs = [df_1, df_2, df_3, df_4, df_5]
 master= pd.DataFrame()
 
+# merges all bot ids to sn=ingle dataframe
 for d in dfs:
     master = master.append(d, ignore_index=True)
 
 user_ids = master['user'].values.tolist()
 
+# save bot ids in txt file
 with open('bots_combined.txt', 'w') as f:
     for item in user_ids:
         f.write("%s\n" % item)
