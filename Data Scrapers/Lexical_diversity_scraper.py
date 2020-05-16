201278f54dcdde574551a392fab33b7b42631392
LuisPettit <62663487+LuisPettit@users.noreply.github.com>
2020-05-16 12:04:20 +0100

    Update Lexical_diversity_scraper.py

--- Data Scrapers/Lexical_diversity_scraper.py
+++ Data Scrapers/Lexical_diversity_scraper.py
@@ -17,36 +17,41 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_key, access_secret)
 api = tweepy.API(auth)
 
-data = pd.read_csv('prepped_data.csv')
-data.head()
+# create connection to db file
+cnx = sqlite3.connect('bots_working.db')
 
-fail_list = []
-final_list = []
-    
-with open('ids.txt', 'r') as file:
-    user_ids = file.read().splitlines()
-    
-    list_length = len(user_ids)
+# read data into dataframes that correspond to classification labels
+df_humans = pd.read_sql_query("SELECT username FROM humans_accounts", cnx)
+df_bots = pd.read_sql_query("SELECT username FROM twitter_users", cnx)
+
+cnx.close()
+
+# create list of user ids
+bot_usernames = df_bots['id'].tolist()
+human_usernames = df_humans['id'].tolist()
+user_ids = bot_usernames + human_usernames    
+list_length = len(user_ids)
     
 api_counter = 0   
 
 counter = 1
 label_count = 0
 
-def luis(twisher, p, test, tweet_text):
+def iterate_tweets(twisher, p, test, tweet_text):
     for tweet in alltweets:
         if p<100:
             try:
                 t = tweet.retweeted_status
             except:
-                
+                # extracts text of tweet
                 tweet_text.append(tweet._json['text'])
-                
+               
                 outtweets = [tweet._json['user']['id_str'], tweet.id, tweet.created_at, tweet._json['text']]
                 test.append(outtweets)
                 p += 1
     return p
 
+# loops through all user ids
 for u_id in user_ids:
         
     start_time = time.time()
@@ -70,7 +75,7 @@ for u_id in user_ids:
         #save most recent tweets
         alltweets.extend(new_tweets)
 
-        val = luis(alltweets, p, test, tweet_text)
+        val = iterate_tweets(alltweets, p, test, tweet_text)
                 
         #print('First tweets collected %d' % val)
 
@@ -95,28 +100,32 @@ for u_id in user_ids:
                 #update the id of the oldest tweet less one
                 oldest = alltweets[-1].id - 1
         
-                val = luis(alltweets, p, test, tweet_text)
+                val = iterate_tweets(alltweets, p, test, tweet_text)
         
 
         df = pd.DataFrame(data=test, columns=['user_id', 'tweet_id', 'Created_at', 'Text'])
         
         print("%d\t--- %s seconds ---" % (counter, time.time() - start_time))
         
-
+    # handles edge case when user account is restrcited
     except:
         print("User not available:%d\t--- %s seconds ---" % (counter, time.time() - start_time))
-        #print('User not available - append to list')
         fail_list.append(u_id)
         
+    # creates single string of all words used in 100 tweets
     mytext = ' '.join(tweet_text)
+    # each word set as individual element in list
     word_list = mytext.split()
+    # punctuation removed along any links
     regex = re.compile('^https?:\/\/.*[\r\n]*')
     okay_items = [x for x in word_list if not regex.match(x)]
     words_n_nums = [t.lower() for t in okay_items if t.isalnum()]
     
+    # lexical diversity calculation
     if len(words_n_nums) == 0:
         val = 0
     else:
+        # distinct words divided by total number of words used
         val = len(set(words_n_nums)) / len(words_n_nums)
         print("\t\t\t\t\t\t%f" % float(val))
         
